d45b0e336f1af99e1637f22edb6e159d56ed5a06
lijianwe <lijianwe@amazon.com>
2024-07-05 20:24:03 +0800

    init commit

--- generative-ai/Module_1_Build_Conversational_Search/bundle_lambda.sh
+++
@@ -1,8 +0,0 @@
-# Download the Langchain module
-cd Module_1_Build_Conversational_Search
-
-aws s3 cp s3://ws-assets-prod-iad-r-pdx-f3b3f9f1a7d6a3d0/2108cfcf-6cd6-4613-83c0-db4e55998757/Langchain.zip .
-
-aws s3 cp s3://ws-assets-prod-iad-r-pdx-f3b3f9f1a7d6a3d0/2108cfcf-6cd6-4613-83c0-db4e55998757/documentEncoder.zip .
-
-aws s3 cp s3://ws-assets-prod-iad-r-pdx-f3b3f9f1a7d6a3d0/2108cfcf-6cd6-4613-83c0-db4e55998757/queryEncoder.zip .
